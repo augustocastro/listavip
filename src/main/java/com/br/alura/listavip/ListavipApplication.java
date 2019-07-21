0cb2993f899941031ebd59d758617ed3491fcd7d
augustocastro <augusto1castro@gmail.com>
2019-07-21 17:23:26 -0300

    Concluindo curso.

--- src/main/java/com/br/alura/listavip/ListavipApplication.java
+++ src/main/java/com/br/alura/listavip/ListavipApplication.java
@@ -1,11 +1,7 @@
 package com.br.alura.listavip;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class ListavipApplication {
@@ -13,14 +9,15 @@ public class ListavipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ListavipApplication.class, args);
 	}
-	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-		dataSource.setUsername("root");
-	    dataSource.setPassword("");
-	    return dataSource;
-	}
+
+//	NOTE: A configuração de conexão com o banco de dados está no (application.properties)	
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
+//		dataSource.setUsername("root");
+//	    dataSource.setPassword("");
+//	    return dataSource;
+//	}
 }
